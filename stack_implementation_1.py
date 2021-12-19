9157df7f103de308a4f06303e7ad077843ea4afc
sai2yeshwanth <yashsai222@gmail.com>
2021-12-20 01:05:09 +0530

    Create stack_implementation_1.py

---
+++ stack_implementation_1.py
@@ -0,0 +1,10 @@
+stack =[] #creating a empty list
+
+stack.append("One") # append() function to push
+stack.append("two")
+stack.append("three")
+
+print(stack)
+
+print(stack.pop()) # three is remove form list
+print(stack)
