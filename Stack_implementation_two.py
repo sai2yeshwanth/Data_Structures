2e508c4f2495e59564aef58ac1d96f33a3aff5cf
sai2yeshwanth <yashsai222@gmail.com>
2021-12-20 11:54:02 +0530

    Update Stack_implementation_two.py

--- Stack_implementation_two.py
+++ Stack_implementation_two.py
@@ -11,9 +11,7 @@ stack.append('c')
 print('Initial stack:')
 print(stack)
  
-# pop() function to pop
-# element from stack in
-# LIFO order
+
 print('\nElements popped from stack:')
 print(stack.pop())
 print(stack.pop())
