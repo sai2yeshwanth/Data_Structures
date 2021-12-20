df07112cd588162ec8649558833e3c7120a42685
sai2yeshwanth <yashsai222@gmail.com>
2021-12-20 18:26:08 +0530

    Create queue_one.py

---
+++ Queue/queue_one.py
@@ -0,0 +1,14 @@
+queue = []
+ 
+# Adding elements to the queue
+queue.append('one') 
+queue.append('two')
+queue.append('three')
+ 
+print(queue)
+ 
+print(queue.pop(0)) #remove element with Indexing 0
+print(queue.pop(0))
+print(queue.pop(0))
+ 
+print(queue)
