20ec193c491eaa6a59b86db2586ae769ccfe8db3
sai2yeshwanth <yashsai222@gmail.com>
2021-12-20 11:58:40 +0530

    Update Stack_implementation_two.py

--- Stack_implementation_two.py
+++ Stack_implementation_two.py
@@ -4,9 +4,9 @@ from collections import deque
 stack = deque()
  
 
-stack.append('a')
-stack.append('b')
-stack.append('c')
+stack.append('one')
+stack.append('two')
+stack.append('two')
  
 print('Initial stack:')
 print(stack)
@@ -16,6 +16,7 @@ print('\nElements popped from stack:')
 print(stack.pop())
 print(stack.pop())
 print(stack.pop())
- 
+
 print('\nStack after elements are popped:')
-print(stack)
+print(stack)    
+#print(stack.pop()) #index error (there no elements in stack)
