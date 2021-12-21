0f222fe08b479d1716300c943ead175c2cc79b62
sai2yeshwanth <yashsai222@gmail.com>
2021-12-21 11:48:48 +0530

    Update linked_list.py

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -7,14 +7,12 @@ class LinkedList:  #creating a linked List
     def __init__(self):
         self.head = None
 
-    def display(self):    #loop
-        if self.head is None:
-            print("List is empty")
-        else:
-            temp = self.head
-            while temp:
-                print(temp.data,"--->", end =" ")
-                temp = temp.next
+    def print(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end="-->")
+            temp = temp.next
+        print()
 
 L = LinkedList() #empty List
 n1 = Node(10)
@@ -22,7 +20,7 @@ L.head = n1 #n1 = L
 n2 = Node(20)
 n1.next = n2 #n2 =n1
 n3 = Node(30)
-n2.next = n3 #n3= n2 
+n2.next = n3 #n3= n2
 n4 = Node(40)
 n3.next =n4 #n4 = n3
-L.display()
+L.print()
